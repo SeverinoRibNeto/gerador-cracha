a793862dade7d9906518c12c0999c4e2cfa61210
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-05-16 22:56:57 -0300

    Create Cracha class

---
+++ src/models/cracha.py
@@ -0,0 +1,9 @@
+from estudante import Estudante
+
+
+class Cracha:
+    def __init__(self, estudante: Estudante) -> None:
+        self.estudante = estudante
+
+    def gerarCracha(self):
+        pass
