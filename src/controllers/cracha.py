f794918c3a23d5ff1c8adf689ac084e358b2b9b4
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-05-22 14:10:36 -0300

    finish method gerar

--- src/controllers/cracha.py
+++ src/controllers/cracha.py
@@ -1,10 +1,10 @@
-import time
 import os
 import threading
 from typing import List
 from models.estudante import Estudante
 from models.cracha import Cracha
 from views.gui import GUI
+from PIL import Image
 from pubsub import pub
 import csv
 import subprocess
@@ -29,6 +29,7 @@ class CrachaController:
                                      pos=wx.DefaultPosition)
 
     def __init__(self):
+        self.gui.contador_lbl.Hide()
 
         if (not os.path.exists('config.txt')):
             # Cria arquivo vazio com a configuração do programa
@@ -72,13 +73,30 @@ class CrachaController:
     def gerar(self, caminho_info, caminho_imagens, pasta_saida):
         # Separar informações do csv para lista e fazer contagem total -ok
         # criar estudante com as informações do csv- ok
-        # Criar lista de arquivos em caminh_imagens
+        # Criar lista de arquivos em caminh_imagens- ok
         # verificar se existe foto do estudante na lista de imagens
         # criar o cracha e preencher as informações
         # salvar na pasta de saidas
-        # adicionar ao contador
-        print(self.lista_imagens(caminho_imagens))
-        pass
+        # adicionar ao contador -ok
+        lista_imagens = self.lista_imagens(caminho_imagens)
+        estudantes = self.cria_lista_estudantes_info(caminho_info)
+        texto = f"Gerando ... (1 de {len(estudantes)})"
+        self.gui.contador_lbl.Show()
+        for contador in range(len(estudantes)):
+            imagem_estudante = self.verifica_estudante_imagens(
+                estudantes[contador],
+                lista_imagens)
+            if imagem_estudante:
+                imagem = Image.open(imagem_estudante)
+                cracha = Cracha(estudantes[contador], imagem_estudante=imagem,
+                                caminho_salvar=pasta_saida)
+            else:
+                cracha = Cracha(estudantes[contador],
+                                caminho_salvar=pasta_saida)
+            cracha.gerar_cracha()
+            cracha = ''
+            self.gui.contador_lbl.SetLabel(
+                texto.replace("(1", "("+str(contador+1)))
 
     def cria_lista_estudantes_info(self, caminho_csv: str) -> List[Estudante]:
         try:
@@ -103,13 +121,25 @@ class CrachaController:
                 for estudante in lista_e
                 if estudante[0] not in self.cabecalho_csv]
 
-    def lista_imagens(self, imagens_dir) -> List[str]:
-        # Percorre o diretório e imagens_dir, percore os arquivos, filtrando eles pelo
-        # formato desejado. Os formatos aceitos são '.jpeg', '.jpg' e '.png'
+    def lista_imagens(self, imagens_dir: str) -> List[str]:
+        # Percorre o diretório e imagens_dir, percore os arquivos,
+        # filtrando eles pelo formato desejado. Os formatos aceitos
+        # são '.jpeg', '.jpg' e '.png'
         return [os.path.join(diretorio, arquivo)
                 for diretorio, subpastas, arquivos in os.walk(imagens_dir)
                 for arquivo in arquivos
                 if any(formato in arquivo for formato in self.formato_imagens)]
+
+    def verifica_estudante_imagens(self, estudante: Estudante,
+                                   lista_imagens: List[str]) -> str:
+        # Procura imagens com o numero do sus do estudante, caso encontre,
+        # retorna a primera imagem encontrada, caso contrario, retorna ''
+        imagens = [imagem for imagem in lista_imagens
+                   if estudante.sus_numero in imagem]
+
+        if len(imagens):
+            return imagens[0]
+        return ''
 
     def criar_arquivo_config(self, caminho_info: str,
                              caminho_imagens: str, pasta_saida: str) -> bool:
